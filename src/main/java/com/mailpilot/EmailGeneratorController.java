fd9935a942beb7d97a5f40d65e5c9a8d01c03ed3
jpriyanshu171 <jpriyanshu171@gmail.com>
2025-09-09 12:17:36 +0530

    MailPilot Backend Initial Commit

--- src/main/java/com/mailpilot/EmailGeneratorController.java
+++ src/main/java/com/mailpilot/EmailGeneratorController.java
@@ -3,16 +3,16 @@ package com.mailpilot;
 import com.mailpilot.EmailGeneratorService;
 import com.mailpilot.EmailRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
-@AllArgsConstructor
 @CrossOrigin(origins = "*")
 public class EmailGeneratorController {
-
-        private final EmailGeneratorService emailGeneratorService;
+        @Autowired
+        private EmailGeneratorService emailGeneratorService;
 
         @PostMapping("/generate")
         public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
